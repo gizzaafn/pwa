882cb0ebc6568d321bc7f18396f97b148698361e
Gizza F Nadifa <geeznotgizz@gmail.com>
2025-01-06 21:45:11 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -19,7 +19,7 @@ const STATIC_ASSETS = [
   'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'
 ];
 
-// Event Instalasi
+
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(STATIC_CACHE)
@@ -30,7 +30,7 @@ self.addEventListener('install', event => {
   );
 });
 
-// Event Aktivasi
+
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => {
@@ -43,7 +43,7 @@ self.addEventListener('activate', event => {
   );
 });
 
-// Event Fetch
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
