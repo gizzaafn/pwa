65284967abe43f692391d9e8839a65e6f3f55f11
Gizza F Nadifa <geeznotgizz@gmail.com>
2025-01-06 21:39:10 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,58 +1,65 @@
-const CACHE_NAME = 'cat-store-cache-v1';
-const urlsToCache = [
+const CACHE_NAME = 'toko-kucing-cache-v1';
+const STATIC_CACHE = 'static-cache-v1';
+const DYNAMIC_CACHE = 'dynamic-cache-v1';
+
+const STATIC_ASSETS = [
   '/',
   '/index.html',
-  '/styles.css',
-  '/script.js',
+  '/manifest.json',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap',
-  'https://storage.googleapis.com/a1aa/image/2SB1qqRwLe2ev0xCx5p4ykakSQfgsOLGrk4GRDYr4gtSASDoA.jpg',
-  'https://storage.googleapis.com/a1aa/image/I4s9MUssgd6UNxKGq5F9myYTsPO1gzPV6VCHZnIr0sGDQaAF.jpg',
-  'https://storage.googleapis.com/a1aa/image/KfJ98WTDYJzAJaZ7yfkm3PXeVBqcChe8J4Cfft7z8ln9CQaAF.jpg',
-  'https://storage.googleapis.com/a1aa/image/GcYVpZIIk9a6B1fqzVwpFODsjXn9nXRPt54fehcagKfeAINgC.jpg',
-  'https://storage.googleapis.com/a1aa/image/l4QizLZPLvZcLhVHU80g8pur89HODq5hI1iXBHa7i1R5QaAF.jpg',
-  'https://i.pinimg.com/736x/d8/57/d5/d857d5fd2701cf843b15adc47fb76ed0.jpg',
-  'https://storage.googleapis.com/a1aa/image/iVtZJoPJTaL3AtnIood5sQ8I6yfe7Arx9LqYu8f4PlEeNkGQB.jpg',
-  'https://storage.googleapis.com/a1aa/image/jZL5Uk9DW551P5CSpGXGIVoGRg9jSy7NVHerG5sN3Cowh0AKA.jpg'
+  'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'
 ];
 
-// Install the service worker and cache resources
+// Event Instalasi
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches.open(STATIC_CACHE)
       .then(cache => {
-        return cache.addAll(urlsToCache);
+        console.log('Menyimpan aset statis ke cache');
+        return cache.addAll(STATIC_ASSETS);
       })
   );
 });
 
-// Fetch and serve cached resources
+// Event Aktivasi
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== STATIC_CACHE && key !== DYNAMIC_CACHE)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Event Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Cache hit - return the cached response
-        if (response) {
-          return response;
+      .then(cacheRes => {
+        return cacheRes || fetch(event.request).then(fetchRes => {
+          return caches.open(DYNAMIC_CACHE).then(cache => {
+            if (!/^https?:\/\/fonts/.test(event.request.url)) {
+              cache.put(event.request.url, fetchRes.clone());
+            }
+            return fetchRes;
+          });
+        });
+      }).catch(() => {
+        if (event.request.url.indexOf('.html') > -1) {
+          return caches.match('/offline.html');
         }
-        return fetch(event.request);
       })
-  );
-});
-
-// Activate the service worker and clean up old caches
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
